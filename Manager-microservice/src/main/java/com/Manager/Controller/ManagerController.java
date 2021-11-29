9e7db47ab9438f25366a63a8bb3009c03ae4e6bd
Venkatesh agnihotri <venkateshagnihotri9926@gmail.com>
2021-11-29 14:12:09 +0530

    Adding the Owner Microservice

---
+++ Manager-microservice/src/main/java/com/Manager/Controller/ManagerController.java
@@ -0,0 +1,15 @@
+package com.Manager.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/manager")
+public class ManagerController {
+
+	@GetMapping("/message")
+	public String test() {
+		return "Hello World From Microservice2";
+	}
+}
